35a61d4c9d12ceb006d0739f8d89ea318fe25ee7
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-02-23 09:06:38 -0300

    Projeto_Gerencimento_Usuarios: linking methods to the interface. creating and editing an user with post and put.

--- client-server/public/classes/HttpRequest.js
+++ client-server/public/classes/HttpRequest.js
@@ -34,7 +34,9 @@ class HttpRequest {
         resolve(obj);
       };
 
-      ajax.send();
+      ajax.setRequestHeader('Content-Type', 'application/json');
+
+      ajax.send(JSON.stringify(params));
     });
   }
 }
--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -1,157 +1,132 @@
 class User {
+  constructor(name, gender, birth, country, email, password, photo, admin) {
+    this._id;
+    this._name = name;
+    this._gender = gender;
+    this._birth = birth;
+    this._country = country;
+    this._email = email;
+    this._password = password;
+    this._photo = photo;
+    this._admin = admin;
+    this._register = new Date();
+  }
 
-    constructor(name, gender, birth, country, email, password, photo, admin){
+  get id() {
+    return this._id;
+  }
 
-        this._id;
-        this._name = name;
-        this._gender = gender;
-        this._birth = birth;
-        this._country = country;
-        this._email = email;
-        this._password = password;
-        this._photo = photo;
-        this._admin = admin;
-        this._register = new Date();
+  get register() {
+    return this._register;
+  }
 
+  get name() {
+    return this._name;
+  }
+
+  get gender() {
+    return this._gender;
+  }
+
+  get birth() {
+    return this._birth;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get photo() {
+    return this._photo;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  get admin() {
+    return this._admin;
+  }
+
+  set photo(value) {
+    this._photo = value;
+  }
+
+  loadFromJSON(json) {
+    for (let name in json) {
+      switch (name) {
+        case "_register":
+          this[name] = new Date(json[name]);
+          break;
+        default:
+            if(name.substring(0,1) === '_') this[name] = json[name];
+      }
+    }
+  }
+
+  static getUsersStorage() {
+    let users = [];
+
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
     }
 
-    get id(){
-        return this._id;
-    }
+    return users;
+  }
 
-    get register(){
-        return this._register;
-    }
+  getNewID() {
+    let usersID = parseInt(localStorage.getItem("usersID"));
 
-    get name(){
-        return this._name;
-    }
+    if (!usersID > 0) usersID = 0;
 
-    get gender() {
-        return this._gender;
-    }
+    usersID++;
 
-    get birth() {
-        return this._birth;
-    }
+    localStorage.setItem("usersID", usersID);
 
-    get country() {
-        return this._country;
-    }
+    return usersID;
+  }
 
-    get email() {
-        return this._email;
-    }
+  toJSON() {
+    let json = {};
 
-    get photo() {
-        return this._photo;
-    }
+    Object.keys(this).forEach((key) => {
+      if (this[key] !== undefined) json[key] = this[key];
+    });
 
-    get password() {
-        return this._password;
-    }
+    return json;
+  }
 
-    get admin() {
-        return this._admin;
-    }
+  save() {
+    return new Promise((resolve, reject) => {
+      let promise;
 
-    set photo(value){
-        this._photo = value;
-    }
+      if (this.id) {
+        promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+      } else {
+        promise = HttpRequest.post(`/users`, this.toJSON());
+      }
 
-    loadFromJSON(json){
+      promise.then((data) => {
+        this.loadFromJSON(data);
 
-        for (let name in json){
-            
-            switch(name){
+        resolve(this);
+      });
+    });
+  }
 
-                case '_register':
-                    this[name] = new Date(json[name]);
-                break;
-                default:
-                    this[name] = json[name];
+  remove() {
+    let users = User.getUsersStorage();
 
-            }
-            
+    users.forEach((userData, index) => {
+      if (this._id == userData._id) {
+        users.splice(index, 1);
+      }
+    });
 
-        }
-
-    }
-
-    static getUsersStorage() {
-
-        let users = [];
-
-        if (localStorage.getItem("users")) {
-
-            users = JSON.parse(localStorage.getItem("users"));
-
-        }
-
-        return users;
-
-    }
-
-    getNewID(){
-
-        let usersID = parseInt(localStorage.getItem("usersID"));
-
-        if (!usersID > 0) usersID = 0;
-
-        usersID++;
-
-        localStorage.setItem("usersID", usersID);
-
-        return usersID;
-
-    }
-
-    save(){
-
-        let users = User.getUsersStorage();
-
-        if (this.id > 0) {
-            
-            users.map(u=>{
-
-                if (u._id == this.id) {
-
-                    Object.assign(u, this);
-
-                }
-
-                return u;
-
-            });
-
-        } else {
-
-            this._id = this.getNewID();
-
-            users.push(this);
-
-        }
-
-        localStorage.setItem("users", JSON.stringify(users));
-
-    }
-
-    remove(){
-
-        let users = User.getUsersStorage();
-
-        users.forEach((userData, index)=>{
-
-            if (this._id == userData._id) {
-
-                users.splice(index, 1);
-
-            }
-
-        });
-
-        localStorage.setItem("users", JSON.stringify(users));
-
-    }
-
+    localStorage.setItem("users", JSON.stringify(users));
+  }
 }
